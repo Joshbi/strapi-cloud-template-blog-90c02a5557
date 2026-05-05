05dea8d70c466f2da1a7f7be6cd14838496608db
Janusz Bielecki <janusz.bielecki@gmail.com>
2026-05-05 09:23:50 +0200

    feat: upload webp

--- config/env/production/plugins.ts
+++ config/env/production/plugins.ts
@@ -1,14 +1,17 @@
 export default ({ env }) => ({
   upload: {
     config: {
-      provider: 'cloudinary',
+      provider: "cloudinary",
       providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
       },
       actionOptions: {
-        upload: {},
+        upload: {
+          fetch_format: "auto",
+          quality: "auto",
+        },
         uploadStream: {},
         delete: {},
       },
